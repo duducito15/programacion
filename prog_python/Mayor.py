e78f3e643d76cd91ce940378265b790c134dc64b
Eduardo <echavez@utea.edu.pe>
2024-10-04 09:53:31 -0500

    Mayor en Python

---
+++ prog_python/Mayor.py
@@ -0,0 +1,7 @@
+numero1 = int(input("Ingresa el primer numero: "))
+numero2 = int(input("Ingresa el segundo numero: "))
+
+if numero1 > numero2:
+    print("El numero " + str(numero1)+" es mayor")
+else:
+    print("El numero " + str(numero2)+" es mayor")
